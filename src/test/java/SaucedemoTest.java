40c9498af20fbdb767c634c9ea6f4541ee744dc0
javian <javianvian@gmail.com>
2026-05-04 23:26:37 +0700

    Update tugas

--- src/test/java/SaucedemoTest.java
+++ src/test/java/SaucedemoTest.java
@@ -7,24 +7,24 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SaucedemoTest {
     WebDriver driver;
-
-    @Test
-    public void positif() {
-        driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com/");
-
-        WebElement nama = driver.findElement(By.id("user-name"));
-        nama.sendKeys("standard_user");
-
-        WebElement pw = driver.findElement(By.id("password"));
-        pw.sendKeys("secret_sauce");
-
-        driver.findElement(By.id("login-button")).submit();
-        String currentUrl = driver.getCurrentUrl();
-        Assertions.assertEquals("https://www.saucedemo.com/inventory.html", currentUrl);
-
-        driver.quit();
-    }
+//
+//    @Test
+//    public void positif() {
+//        driver = new ChromeDriver();
+//        driver.get("https://www.saucedemo.com/");
+//
+//        WebElement nama = driver.findElement(By.id("user-name"));
+//        nama.sendKeys("standard_user");
+//
+//        WebElement pw = driver.findElement(By.id("password"));
+//        pw.sendKeys("secret_sauce");
+//
+//        driver.findElement(By.id("login-button")).submit();
+//        String currentUrl = driver.getCurrentUrl();
+//        Assertions.assertEquals("https://www.saucedemo.com/inventory.html", currentUrl);
+//
+//        driver.quit();
+//    }
 
     @Test
     public void negatif() {
@@ -41,6 +41,37 @@ public class SaucedemoTest {
         String teksError = pesanError.getText();
 
         Assertions.assertTrue(teksError.contains("Username and password do not match"));
+
+        driver.quit();
+    }
+    @Test
+    public void positif2() {
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+
+        //menggunakan Locator NAME
+        WebElement nama = driver.findElement(By.name("user-name"));
+        nama.sendKeys("standard_user");
+
+        //menggunakan Locator XPATH dengan pendekatan AXES
+        //Axes: parent:: (naik ke pembungkus) dan following-sibling:: (cari saudara sebelahnya)
+        WebElement pw = driver.findElement(By.xpath("//input[@name='user-name']/parent::div/following-sibling::div/input"));
+        pw.sendKeys("secret_sauce");
+
+        //menggunakan Locator CLASS
+        WebElement btnLogin = driver.findElement(By.className("submit-button"));
+        btnLogin.click();
+
+        String currentUrl = driver.getCurrentUrl();
+        Assertions.assertEquals("https://www.saucedemo.com/inventory.html", currentUrl);
+
+        //menggunakan Locator XPATH dengan pendekatan FUNCTION
+        WebElement logoElement = driver.findElement(By.xpath("//div[contains(text(), 'Swag Labs')]"));
+        Assertions.assertTrue(logoElement.isDisplayed(), "Teks Swag Labs tidak ditemukan!");
+
+        //menggunakan Locator TAG
+        WebElement gambarProduk = driver.findElement(By.tagName("img"));
+        Assertions.assertTrue(gambarProduk.isDisplayed(), "Gambar produk tidak dirender!");
 
         driver.quit();
     }
